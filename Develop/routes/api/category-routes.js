3ebf88b1252f017b568bbc272cf2c34904882ff8
miacarmen <maitreyacarmen@gmail.com>
2022-05-03 23:58:56 -0700

    all routes added

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -57,7 +57,7 @@ router.put("/:id", async (req, res) => {
       },
     });
     if(!categoryData) {
-      res.status(404).json({ message: 'No category with that id' });
+      res.status(404).json({ message: 'No category found with that id' });
       return;
     }
     res.status(200).json(categoryData);
